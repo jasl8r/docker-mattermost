26465c80feacdc3d6f0119ebea96bf98e1c795ab
Florian Demesmaeker <florian.demesmaeker@hotmail.fr>
2018-04-27 16:43:26 +0200

    Upgrade to 4.9.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ case ${1} in
     initialize
     configure
     migrate
-    ./bin/platform -config ${MATTERMOST_CONF_DIR}/config.json
+    ./bin/platform --config ${MATTERMOST_CONF_DIR}/config.json
     ;;
 
   app:migrate)
